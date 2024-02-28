afdfef09957c4556944a5d07be44d64e3241ec18
levibarra <157933526+levibarra@users.noreply.github.com>
2024-02-27 21:13:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//como eu resolvi
+var nomePessoa = 
+
+window.prompt('Digite seu nome')
+
+alert('Seja bem vindo(a), ' + nomePessoa + '!')
+
+/*
+como o professor resolveu
+
+
+const nomeDoUsuario = prompt('Digite seu nome: ')
+
+alert('Seja bem vindo(a), ' + nomeDoUsuario + '!')
+
+ele diz que o ideal é não usar window e não usar var e sim const ao atribuir um objeto
+*/
